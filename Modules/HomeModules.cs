e865153e2e5112f242c018b65d581f6bd5a99c35
David Wilson and Guy Anderson <david+guy@>
2017-05-23 12:05:35 -0700

    add more entry fields for the car constructor to make full objects, allow the user to view full details of each created object

--- Modules/HomeModules.cs
+++ Modules/HomeModules.cs
@@ -10,13 +10,17 @@ namespace cardealership
     {
       Get["/"] = _ => View["add_new_car.cshtml"];
       Get["/view_all_cars"] = _ =>{
-        List<string> allCars = Car.GetAll();
+        List<Car> allCars = Car.GetAll();
         return View["view_all_cars.cshtml", allCars];
       };
       Post["/car_added"] = _ => {
-        Car newCar = new Car (Request.Form["new-car"]);
+        Car newCar = new Car (Request.Form["new-car"],Request.Form["new-car-model"],Request.Form["new-car-year"],Request.Form["new-car-price"], Request.Form["new-car-mileage"]);
         newCar.Save();
         return View["cars_added.cshtml", newCar];
+      };
+      Post["/cars_cleared"] = _ => {
+        Car.ClearAll();
+        return View["cars_cleared.cshtml"];
       };
     }
   }
--- Objects/Car.cs
+++ Objects/Car.cs
@@ -5,42 +5,76 @@ namespace cardealer.Objects
   public class Car
   {
     private string _description;
-    // private string _model;
-    private static List<string> _instances = new List<string> {};
+    private string _model;
+    private string _year;
+    private string _price;
+    private string _mileage;
+    private static List<Car> _instances = new List<Car> {};
 
 // CONSTRUCTOR
-    public Car (string description)
+    public Car (string description, string model, string year, string price, string mileage)
     {
       _description = description;
-      // _model = model;
+      _model = model;
+      _year = year;
+      _price = price;
+      _mileage = mileage;
     }
 // RETURNS VARIABLE
     public string GetDescription()
     {
       return _description;
     }
-    // public string GetModel()
-    // {
-    //   return _model;
-    // }
+    public string GetModel()
+    {
+      return _model;
+    }
+    public string GetYear()
+    {
+      return _year;
+    }
+    public string GetPrice()
+    {
+      return _price;
+    }
+    public string GetMileage()
+    {
+      return _mileage;
+    }
 // SETS VARIABLE
     public void SetDescription(string newDescription)
     {
       _description = newDescription;
     }
-    // public void SetModel(string newModel)
-    // {
-    //   _model = newModel;
-    // }
+    public void SetModel(string newModel)
+    {
+      _model = newModel;
+    }
+    public void SetYear(string newYear)
+    {
+      _year = newYear;
+    }
+    public void SetPrice(string newPrice)
+    {
+      _price = newPrice;
+    }
+    public void SetMileage(string newMileage)
+    {
+      _price = newMileage;
+    }
 //RETURNS LIST
-    public static List<string> GetAll()
+    public static List<Car> GetAll()
     {
       return _instances;
     }
 //SAVES INSTANCES IN LIST
     public void Save()
     {
-      _instances.Add(_description);
+      _instances.Add(this);
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
     }
   }
 }
